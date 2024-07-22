3fc62b5c63c305667b37605fcca6330b2263f31b
xqdt <oxymususer@gmail.com>
2024-07-22 11:38:00 +0500

    added possibility send quotes

---
+++ main.py
@@ -0,0 +1,51 @@
+import config
+import telebot
+from telebot import types
+import requests
+
+# создаём экземпляр бота
+bot = telebot.TeleBot(config.API_TOKEN)
+
+# вспомогательные переменные
+lang = None
+response = None
+url = "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang="
+
+# Обработчик /start команды
+@bot.message_handler(commands=["start"])
+def welcome(message):
+	# создаём инлайн-кнопки
+	markup = types.ReplyKeyboardMarkup(row_width=2)
+	button_en_lang = types.KeyboardButton("🇬🇧English")
+	button_ru_lang = types.KeyboardButton("🇷🇺Русский")
+	markup.add(button_en_lang, button_ru_lang)
+
+	# Отправляем сообщение с кнопками
+	bot.send_message(message.chat.id, "Выберите язык", reply_markup=markup)
+
+# Обработчик сообщений
+@bot.message_handler(func=lambda message: True)
+def quote_message(message):
+	global lang
+
+	if message.text == "🇬🇧English":
+		lang = "en"
+	elif message.text == "🇷🇺Русский":
+		lang = "ru"
+
+	send_quote(message)
+
+# Отправляет цитату
+def send_quote(message):
+	response = requests.get(url + lang)
+
+	if response.status_code == 200:
+		quoteObj = response.json()
+
+		bot.send_message(message.chat.id, f"<i>{quoteObj["quoteText"]}</i>\n\n<b>{quoteObj["quoteAuthor"]}</b>", parse_mode="HTML")
+	else:
+		print(f"Ошибка: {response.status_code}")
+
+# Запуск бота
+if __name__ == "__main__":
+	bot.polling()
